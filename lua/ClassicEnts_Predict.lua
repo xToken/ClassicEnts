248345fba6b11c6f9334e6dacf31b5847d6e3c8c
Matt <mkreider@gmail.com>
2015-07-02 00:28:26 -0400

    New ents and files + comments

--- lua/ClassicEnts_Predict.lua
+++ lua/ClassicEnts_Predict.lua
@@ -1,8 +1,12 @@
-// 
-// 
+// Natural Selection 2 'Classic Entities Mod'
+// Adds some additional entities inspired by Half-Life 1 and the Extra Entities Mod by JimWest - https://github.com/JimWest/ExtraEntitesMod
+// Designed to work with maps developed for Extra Entities Mod.  
+// Source located at - https://github.com/xToken/ClassicEnts
 // lua\ClassicEnts_Predict.lua
 // - Dragon
 
+Script.Load("lua/ClassicEnts/CheatyThings.lua")
+Script.Load("lua/ClassicEnts/BreakableEmitter.lua")
 Script.Load("lua/ClassicEnts/ControlledButtonEmitter.lua")
 Script.Load("lua/ClassicEnts/ControlledMoveable.lua")
 Script.Load("lua/ClassicEnts/ControlledWeldableEmitter.lua")
